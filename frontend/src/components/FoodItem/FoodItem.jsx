a3398167f7a7e85ec6e4a8f612f7f18c3fd1a164
Owais Lone <ovilone92@gmail.com>
2026-05-25 01:26:03 +0530

    feat - Revamp FoodItem component with enhanced styling, improved layout, and responsive design for better user experience

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,37 +1,58 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/frontend_assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const {cartItems,addToCart,removeFromCart,url}=useContext(StoreContext); 
+  const { cartItems, addToCart, removeFromCart, url } =
+    useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img src={url+"/images/"+image} alt="" className="food-item-image" />
+        <img
+          src={url + "/images/" + image}
+          alt={name}
+          className="food-item-image"
+        />
+
         {!cartItems[id] ? (
-          <img
-            className="add"
-            onClick={() => addToCart(id)}
-            src={assets.add_icon_white}
-            alt=""
-          />
+          <button className="add-btn" onClick={() => addToCart(id)}>
+            <img src={assets.add_icon_white} alt="" />
+          </button>
         ) : (
           <div className="food-item-counter">
-            <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+
             <p>{cartItems[id]}</p>
-            <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
           </div>
         )}
       </div>
+
       <div className="food-item-info">
         <div className="food-item-name-rating">
-          <p>{name}</p>
+          <h3>{name}</h3>
+
           <img src={assets.rating_starts} alt="" />
         </div>
+
         <p className="food-item-desc">{description}</p>
-        <p className="food-item-price">₹{price}</p>
+
+        <div className="food-item-bottom">
+          <p className="food-item-price">₹{price}</p>
+
+          <span className="food-item-tag">Bestseller</span>
+        </div>
       </div>
     </div>
   );
